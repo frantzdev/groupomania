85cbc9f79f9b323b6f39710f492ff5c10db8ec5c
Frantz <frantz.neveu@sfr.fr>
2021-04-15 13:30:35 +0200

    Ajout page d'accueil

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="container-full">
+    <div class="row text-center">
+      <div class="col">
+        <h1 class="my-5">Réseau social Groupomania</h1>
+        <img class="mx-auto d-block w-50 my-3" src="../../public/logos/capture.png" alt="Siege de l'entreprise">
+      </div>
+    </div>
+
+    <div class="row my-5">
+      <div class="col text-center">
+        <h3 class="my-5">Rejoignez-nous dès maintenant</h3>
+        <div>
+          <a class="btn btn-secondary col-lg-2 col-md-4 col-6 mb-3 shadow-lg" role="button"
+            title="S'inscrire">Inscription</a>
+        </div>
+        <div>
+          <a class="btn btn-secondary col-lg-2  col-md-4 col-6 my-3 shadow-lg" role="button"
+            title="Connectez-vous">Connexion</a>
+        </div>
+      </div>
+    </div>
+
+    <footer class="footer">
+      <p>Groupomania Siege social 18 route des champs 48720 Salbinor</p>
+    </footer>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
