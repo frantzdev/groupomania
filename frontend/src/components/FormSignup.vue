d376cfc3daff59c2412203dbaf4f6fba9883f276
Frantz <frantz.neveu@sfr.fr>
2021-04-19 12:12:17 +0200

    Ajout des regex

--- frontend/src/components/FormSignup.vue
+++ frontend/src/components/FormSignup.vue
@@ -8,33 +8,31 @@
         </div>
       </div>
 
+
       <div class="row">
         <div class="col-10 mx-auto">
           <form>
             <div class="mb-3">
               <label for="InputFirstName" class="form-label"></label>
-              <input type="text" class="form-control" id="InputFirstName" placeholder="Prénom"
-                pattern="^[A-Za-z\é\è\ê\ -]{2,30}$', 'g" v-model="firstname">
-              <small></small>
+              <input type="text" class="form-control" id="InputFirstName" placeholder="Prénom" v-model="firstname">
+              <small v-if="validFirstname(firstname)">error</small>
 
               <label for="InputLastName" class="form-label"></label>
-              <input type="text" class="form-control" id="InputLastName" placeholder="Nom"
-                pattern="^[A-Za-z\é\è\ê\ -]{2,30}$', 'g" v-model="lastname">
+              <input type="text" class="form-control" id="InputLastName" placeholder="Nom" v-model="lastname">
               <small></small>
 
               <label for="InputEmail" class="form-label"></label>
-              <input type="email" class="form-control" id="InputEmail" placeholder="Email"
-                pattern="^[a-zA-Z0-9-._]+[@]{1}[a-zA-Z0-9-._]+[.]{1}[a-z]{2,10}$" v-model="email">
+              <input type="email" class="form-control" id="InputEmail" placeholder="Email" v-model="email">
               <small></small>
-        
+
               <label for="InputPassword" class="form-label"></label>
-              <input type="password" class="form-control" id="InputPassword" placeholder="Mot de passe"
-                pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" v-model="password">
+              <input type="password" autocomplete="on" class="form-control" id="InputPassword"
+                placeholder="Mot de passe" v-model="password">
               <small></small>
             </div>
-              <router-link to="/community">
-                <a class="btn btn-lg bg fw-bold col-12 my-5" role="button" type="submit" @click="signup()">{{text}}</a>
-              </router-link>
+            <router-link to="/community">
+              <a class="btn btn-lg bg fw-bold col-12 my-5" role="button" type="submit" @click="signup()">{{text}}</a>
+            </router-link>
           </form>
         </div>
       </div>
@@ -52,7 +50,6 @@
   export default {
     name: 'FormSignup',
     components: {
-      // Button,
       LogoForm,
       LogoRight
     },
@@ -66,27 +63,43 @@
         lastname: "",
         email: "",
         password: "",
-        msg: []    
       }
     },
-    
+
     methods: {
       signup() {
-            console.log("envoi inscription")
-            axios.post('http://localhost:3000/api/auth/signup', {
-                            firstname: this.firstname,
-                            lastname: this.lastname,
-                            email: this.email,
-                            password: this.password
-                        })
-                        .then(response => {
-                            console.log(response);
-                        })
-                        // .catch(error => {
-                        //     console.log('error')
-                        // });                     
-        }  
-    }
+        console.log("envoi inscription")
+        axios.post('http://localhost:3000/api/auth/signup', {
+            firstname: this.firstname,
+            lastname: this.lastname,
+            email: this.email,
+            password: this.password
+          })
+          .then(response => {
+            console.log(response);
+          })
+        // .catch(error => {
+        //     console.log('error')
+        // });  
+
+      },
+
+      validFirstname(firstname) {
+        let regexFirstname = /^[A-Za-zéèê'à -]{2,30}$/;
+        return regexFirstname.test(firstname);
+      },
+      validLastname(lastname) {
+        let regexLastname = /^[A-Za-zéèê'à -]{2,30}$/;
+        return regexLastname.test(lastname);
+      },
+      validEmail(email) {
+        let regexEmail = /(.+)@(.+){2,}\.(.+){2,}/;
+        return regexEmail.test(email);
+      },
+      validPassword(password) {
+        let regexPassword = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+        return regexPassword.test(password);
+      }
+    } 
   }
 </script>
-
