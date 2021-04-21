dffeffe0bfb9541c104cf11de97bed9c43097090
Frantz <frantz.neveu@sfr.fr>
2021-04-21 14:57:40 +0200

    bcrypt fonctionnel sur signup et login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -17,10 +17,21 @@ exports.signup = async (req, res, next) => {
     .catch(error => res.status(400).json({ error: error }));
 };
 
-exports.login = (req, res, next) => {
-  models.User.findOne({ where: { email: req.body.email }})
-  return res.json({message: "connexion"});    
-};  
+exports.login = async (req, res, next) => {
+    const userDb = await models.User.findOne({ where: { email: req.body.email } });
+      if (!userDb) {  //si l'utilisateur n'est pas dans la base
+        console.log('NON trouvé!'); 
+        return res.status(401).json({ message: 'Utilisateur non trouvé !' });
+      } //sinon l'utilisateur est dans la base
+        console.log('trouvé !')
+        const bcryptCompare = await bcrypt.compare(req.body.password, userDb.password);
+          if (!bcryptCompare) { //si le hash n'est PAS identique
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          } else {  //sinon le hash est identique, je conserve else pour me repérer
+            return res.status(200).json({ message: 'Utilisateur trouvé !' });
+            }   
+}; 
+ 
     
 
 
